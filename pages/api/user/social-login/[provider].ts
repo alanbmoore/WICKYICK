f574a40630257c53c3bbafd3051398a4c652d1c3
Michael Griffin <mike@munchdevelopment.com>
2022-07-07 02:16:24 -0500

    build error

--- pages/api/user/social-login/[provider].ts
+++ pages/api/user/social-login/[provider].ts
@@ -28,8 +28,8 @@ export default async function handler(
     let profile;
 
     const isProfileCreated = await isProfileCreatedForUser(body.user.user.uid);
-    if (!isProfileCreated) {
-      const names = user.displayName.split(" ");
+    if (!isProfileCreated && user) {
+      const names = (user.displayName || "").split(" ");
       let userProfile = <IUser>{
         first_name: names.length > 0 ? names[0] : null,
         last_name: names.length > 1 ? names[1] : null,
